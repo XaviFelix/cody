ed7125c06989bffe6f209b872882d284a7a65b00
xfelix <xavierfelix74@gmail.com>
2025-02-13 23:25:04 -0800

    changed to gridlayout

--- frontend/Setup.py
+++ frontend/Setup.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QComboBox, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QComboBox, QGridLayout
+from components import GroupCheckBoxes
 
 class Setup(QWidget):
     def __init__(self):
@@ -7,17 +8,29 @@ class Setup(QWidget):
         self.comboBox = QComboBox()
         self.comboBox.addItems(["Java", "Python", "Go", "Rust", "C"])
 
-        vLayout = QVBoxLayout()
-        vLayout.addWidget(self.comboBox)
-        self.setLayout(vLayout)
+        # List of programming language topics (general),
+        #TODO: curate topics specific to PL
+        self.topics = [
+            "Syntax and Semantics", 
+            "Datatypes and Semantics", 
+            "Control Flow", 
+            "Functions and Scope"
+        ]
+        self.checkBoxes = GroupCheckBoxes(self.topics)
+        self.checkBoxes2 = GroupCheckBoxes(self.topics)
+        self.checkBoxes3 = GroupCheckBoxes(self.topics)
+        self.checkBoxes4 = GroupCheckBoxes(self.topics)
 
-    # creates a list of radio buttons that will affect
-    # how the ai will ask questions to the user
+        gridLayout = QGridLayout()
+        
+        # widget, row, column 
+        gridLayout.addWidget(self.comboBox, 0, 0)
+        gridLayout.addWidget(self.checkBoxes, 1, 0)
+        gridLayout.addWidget(self.checkBoxes2, 1, 1)
+        gridLayout.addWidget(self.checkBoxes3, 2, 0)
+        gridLayout.addWidget(self.checkBoxes4, 2, 1)
 
-    # could create a general init for various languages
-    # just need to pass the name of the pl as an argument
-    # def initJavaPrinciples(self):
-
+        self.setLayout(gridLayout)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
