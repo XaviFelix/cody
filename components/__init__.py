968c156c8e82232d2838792823b5b6a0520082e3
xfelix <xavierfelix74@gmail.com>
2025-02-13 23:24:24 -0800

    checkbox widget to module

--- components/__init__.py
+++ components/__init__.py
@@ -2,5 +2,6 @@ from .ClearButton import ClearButton
 from .EnterButton import EnterButton
 from .InputBox import InputBox
 from .DisplayBox import DisplayBox
+from .GroupCheckBoxes import GroupCheckBoxes
 
 __all__ = ['ClearButton', 'EnterButton', 'InputBox', 'DisplayBox'] 
