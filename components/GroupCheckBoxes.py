6dfc28ad08cdefc7c336e5a06c9e4bb32ba3d404
xfelix <xavierfelix74@gmail.com>
2025-02-15 17:10:37 -0800

    improved grouped checkBoxes

--- components/GroupCheckBoxes.py
+++ components/GroupCheckBoxes.py
@@ -2,8 +2,8 @@ import sys
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QCheckBox, QGroupBox
 
 class GroupCheckBoxes(QGroupBox):
-    def __init__(self, listOfCheckBoxNames):
-        super().__init__("Core Principles")
+    def __init__(self, listOfCheckBoxNames, principles): # pass a string named, principle
+        super().__init__(principles) # We need to let this be passed by the instance creation
         self.setCheckable(True)
 
         self.layout = QVBoxLayout()
@@ -31,6 +31,6 @@ class GroupCheckBoxes(QGroupBox):
 # using for testing this component
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = GroupCheckBoxes(["Syntax and Semantics", "Datatypes and Semantics", "Control Flow", "Functions and Scope"])
+    window = GroupCheckBoxes(["Syntax and Semantics", "Datatypes and Semantics", "Control Flow", "Functions and Scope"], "Hello")
     window.show()
     sys.exit(app.exec())
