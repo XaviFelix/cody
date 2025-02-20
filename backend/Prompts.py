2e8790ae97be2442d7501c88b1a69ca1036c436e
xfelix <xavierfelix74@gmail.com>
2025-02-20 00:00:04 -0800

    new method

---
+++ backend/Prompts.py
@@ -0,0 +1,11 @@
+
+# This needs to append the list to the main prompt
+# TODO: Create a main prompt
+# get main propmt using env var and store into a string
+# then append from there
+
+def promptLLM(selected_options):
+    appnedToMainPrompt = " ".join(selected_options)
+
+    print("These are the words coming from promptLLM():")
+    print(appnedToMainPrompt)
