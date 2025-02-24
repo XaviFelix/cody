2af19b05fcc33f9425e8cf2f20424294dda1b4bc
xfelix <xavierfelix74@gmail.com>
2025-02-23 22:54:25 -0800

    cleaned up code

--- components/CopyButton.py
+++ components/CopyButton.py
@@ -1,20 +1,14 @@
 from PyQt6.QtWidgets import QApplication, QPushButton
 from .InputBox import InputBox
-import sys
 
 class CopyButton(QPushButton):
     def __init__(self, label: str, inputBox: InputBox, parent=None):
         super().__init__(label, inputBox)
 
         self.inputBox = inputBox
-
         self.setToolTip("Copy Text")
-        self.clicked.connect(self.handleCopy) # I don't really need this unless i am testing here
+        self.clicked.connect(self.handleCopy)
 
-
-    # Test this behavior
     def handleCopy(self):
-        # Add a functionality to where if the text is empty
-        # Then it won't copy to the clipboard
-        clipboard= QApplication.clipboard()
+        clipboard = QApplication.clipboard()
         clipboard.setText(self.inputBox.toPlainText())
