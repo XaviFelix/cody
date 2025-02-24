ae8c2f9b7e5d8dbb8e7d8c582cffc1d093e2d193
xfelix <xavierfelix74@gmail.com>
2025-02-23 22:56:57 -0800

    updated

--- components/GroupCheckBoxes.py
+++ components/GroupCheckBoxes.py
@@ -1,10 +1,9 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QCheckBox, QGroupBox
-from backend import promptLLM 
 
 class GroupCheckBoxes(QGroupBox):
     def __init__(self, listOfCheckBoxNames, principles): # pass a string named, principle
-        super().__init__(principles) # We need to let this be passed by the instance creation
+        super().__init__(principles)
         self.setCheckable(True)
 
         self.layout = QVBoxLayout()
@@ -25,15 +24,8 @@ class GroupCheckBoxes(QGroupBox):
     def on_checkbox_state_changed(self, state):
         selected_options = [current.text() for current in self.check_boxes if current.isChecked()]
         print("This is the selected options: ", selected_options)
-        promptLLM(selected_options)
-
-        # call a function from the prompt file
-        # which will use the server file to append
-        # the data from these lists into the pr
         
-
-        
-# using for testing this component
+# used for testing this component
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = GroupCheckBoxes(["Syntax and Semantics", "Datatypes and Semantics", "Control Flow", "Functions and Scope"], "Hello")
