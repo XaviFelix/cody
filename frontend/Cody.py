53602259416597c920bb9cac6b5312c5b186bf9a
xfelix <xavierfelix74@gmail.com>
2025-02-23 22:59:12 -0800

    fixed logic errors

--- frontend/Cody.py
+++ frontend/Cody.py
@@ -1,7 +1,5 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout
 from components import ClearButton, EnterButton, InputBox, DisplayBox, CopyButton, PasteButton
-from backend import startCody
-import threading
 
 class Cody(QWidget):
     def __init__(self):
@@ -17,9 +15,9 @@ class Cody(QWidget):
         self.input_field.setPlaceholderText("Enter some text here...")
         self.display_field = DisplayBox()
 
-        # I need to pass the input_field here to the buttons so that 
-        # THey can refernce it and do their respective methods
-        self.enter_button = EnterButton("Enter", self.input_field)
+        # TODO: Now i need to pass a ref to the display so that i can display the 
+        #       information
+        self.enter_button = EnterButton("Enter", self.input_field, self.display_field)
         self.clear_button = ClearButton("Clear", self.input_field)
         self.paste_button = PasteButton("Paste", self.input_field)
         self.copy_button = CopyButton("Copy", self.input_field)
@@ -36,13 +34,3 @@ class Cody(QWidget):
         self.vertical_layout.addWidget(self.input_field)
         self.vertical_layout.addLayout(horizontal_layout)
         self.setLayout(self.vertical_layout)
-
-        # connect signals
-        self.enter_button.clicked.connect(self.enter_button.handleEnter)
-        self.clear_button.clicked.connect(self.clear_button.handleClear)
-        self.paste_button.clicked.connect(self.paste_button.handlePaste)
-        self.copy_button.clicked.connect(self.copy_button.handleCopy)
-
-        # Start Cody here:
-        thread =threading.Thread(target=startCody, args=(self.display_field, self.enter_button))
-        thread.start()
