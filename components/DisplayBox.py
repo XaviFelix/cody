322e13bb61f8cd693625127faf470b71e14e69de
xfelix <xavierfelix74@gmail.com>
2025-02-23 22:55:42 -0800

    updated references and button handler

--- components/DisplayBox.py
+++ components/DisplayBox.py
@@ -1,6 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QTextEdit 
 from PyQt6.QtGui import QFont
-import sys
 
 class DisplayBox(QTextEdit):
     def __init__(self):
@@ -10,13 +9,5 @@ class DisplayBox(QTextEdit):
         self.font = QFont("Arial", 16)
         self.setFont(self.font)
 
-
-
-    def updateDisplay(self):
-        self.setText("Welcome!")
-       
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    demo = DisplayBox()
-    demo.show()
-    sys.exit(app.exec())
+    def updateDisplay(self, completion):
+        self.setText(completion)
