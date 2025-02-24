48ea18be5a46640dd1398ad0b7d9b3ded1d96a6d
xfelix <xavierfelix74@gmail.com>
2025-02-23 22:58:01 -0800

    updated paste button

--- components/PasteButton.py
+++ components/PasteButton.py
@@ -1,14 +1,14 @@
 from PyQt6.QtWidgets import QApplication, QPushButton
 from .InputBox import InputBox
-import sys
 
 class PasteButton(QPushButton):
     def __init__(self, label: str, inputBox: InputBox, parent=None):
         super().__init__(label, inputBox)
 
         self.inputBox = inputBox
-
         self.setToolTip("Paste")
+
+        self.clicked.connect(self.handlePaste)
 
     def handlePaste(self): 
         print("Pasting from clipboard")
